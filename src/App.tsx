c4abc65e498a8814140935126d87ae974fbcbe9d
icenapi <miradadesigual@gmail.com>
2024-10-14 22:00:52 +0200

    Add route system

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import Listing from './pages/Listing/Listin';
+import { Route, Routes } from 'react-router-dom';
+import Chapter from './pages/Chapter/Chapter';
+import Details from './pages/Details/Details';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path='' element={<Listing />} />
+        <Route path='' element={<Chapter />} />
+        <Route path='' element={<Details />} />
+      </Routes>
     </div>
   );
 }
