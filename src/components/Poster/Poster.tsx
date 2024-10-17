39976ee0ff87cbda5de297b174cf916de7710b4a
icenapi <miradadesigual@gmail.com>
2024-10-17 17:55:21 +0200

    Poster display for the 100 list

--- src/components/Poster/Poster.tsx
+++ src/components/Poster/Poster.tsx
@@ -1,13 +1,20 @@
-import { TPodcast } from "../../Models/TPodcast";
+import { Link } from "react-router-dom";
+import { TPoster } from "../../Models/TPodcast";
 
 const BASE_CLASS = "poster"
 
-const Poster = (posterInfo: TPodcast) => {
+const Poster = (posterInfo: TPoster) => {
+
     return (
-        <div className={BASE_CLASS}>
-            <img src={posterInfo.src} alt={posterInfo.author} title={posterInfo.title} />
-            <h1>{posterInfo.title}</h1>
-            <span>{posterInfo.author}</span>
+        <div className={BASE_CLASS} key={posterInfo.podcastId}>
+            <div className={BASE_CLASS + '__image'}>
+                <img src={posterInfo.src} alt={posterInfo.author} title={posterInfo.name} />
+            </div>
+            <Link 
+                to={`/podcast/${posterInfo.podcastId}`}>
+                    {(posterInfo.name).toUpperCase()}
+            </Link>
+            <span>Author: {posterInfo.author}</span>
         </div>
     )
 }
