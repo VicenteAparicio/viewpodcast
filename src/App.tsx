b12db3f5c497b94d614e79540fc2c2fc8f01b167
icenapi <miradadesigual@gmail.com>
2024-10-17 17:54:10 +0200

    Rename some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
-import Listing from './pages/Listing/Listin';
 import { Route, Routes } from 'react-router-dom';
-import Chapter from './pages/Chapter/Chapter';
-import Details from './pages/Details/Details';
+import Episode from './pages/Episode/Episode';
+import Podcast from './pages/Podcast/Podcast';
+import { Provider } from 'react-redux';
+import Store from './Store/store';
+import PodcastList from './pages/PodcastList/PodcastList';
 
-function App() {
+const BASE_CLASS = "App"
+
+const App = () => {
+
   return (
-    <div className="App">
+    <Provider store={Store}>
+    <div className={BASE_CLASS}>
       <Header />
+      
       <Routes>
-        <Route path='' element={<Listing />} />
-        <Route path='' element={<Chapter />} />
-        <Route path='' element={<Details />} />
+        <Route path='' element={<PodcastList />} />
+        <Route path='/podcast/:podcastId' element={<Podcast />} />
+        <Route path='/podcast/:podcastId/episode/:episodeId' element={<Episode />} />
       </Routes>
     </div>
+    </Provider>
   );
 }
 
