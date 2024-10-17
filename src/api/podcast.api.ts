dec1609edbe617b50b47e91e25210eb93319422d
icenapi <miradadesigual@gmail.com>
2024-10-17 17:50:17 +0200

    Fix call to get podcast details

--- src/api/podcast.api.ts
+++ src/api/podcast.api.ts
@@ -1,6 +1,8 @@
 
 import axios from 'axios';
+import { XMLParser } from 'fast-xml-parser';
 
+const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/'
 const URL_LIST = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json';
 const URL_ALBUM = 'https://itunes.apple.com/lookup?id=';
 
@@ -15,9 +17,8 @@ export const fetchAllPodcasts = async () => {
 }
 
 export const fetchAlbum = async (podcastId: number) => {
-
     try {
-        return (await axios.get(`${URL_ALBUM}${podcastId}`)).data.results[0];
+        return (await axios.get(CORS_ANYWHERE + URL_ALBUM + podcastId + '&media=podcast&entity=podcastEpisode')).data.results;
 
     } catch (err) {
         console.error(err);
