d420706ca2eb55c33539931bcc0d8c6bb24018e8
icenapi <miradadesigual@gmail.com>
2024-10-17 17:58:14 +0200

    Scss imports

---
+++ src/pages/Episode/Episode.tsx
@@ -0,0 +1,13 @@
+
+const BASE_CLASS = "episode"
+
+const Episode = () => {
+
+    return (
+        <div className={BASE_CLASS}>
+            Episode
+        </div>
+    )
+}
+
+export default Episode;
